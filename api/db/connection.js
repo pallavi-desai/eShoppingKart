17386ec6f3ffb1350a2ece3952cf3f381cfd3fc0
pallavi <pl561741@dal.ca>
2020-09-07 16:59:52 -0300

    changing connection string

--- api/db/connection.js
+++ api/db/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 
-const url = "mongodb+srv://webUser:admin123@cluster0.jb5qq.mongodb.net/CSCI_5709_Project?retryWrites=true&w=majority"
+const url = "give your mongodb connection string"
 
 mongoose.connect(url,{
     useNewUrlParser: true,
